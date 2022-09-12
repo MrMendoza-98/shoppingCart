f31199295a970d50ccd7798949f1647ac81355e2
Miguel Mendoza <36460814+MrMendoza-98@users.noreply.github.com>
2022-09-12 15:42:05 -0500

    Add views livewire

--- resources/views/livewire/cart-update.blade.php
+++ resources/views/livewire/cart-update.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <input wire:model="quantity"
+           type="number" min="1" max="5"
+           wire:change="updateCart" class="text-center bg-gray-100">
 </div>
